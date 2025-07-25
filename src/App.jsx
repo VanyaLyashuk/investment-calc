783ad0b1b96d76e040d89aeff6ddaae37716568d
VanyaLyashuk <ivan129522@gmail.com>
2025-07-25 08:52:50 +0300

    feat(app): render UserInput component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import UserInput from "./components/userInput/UserInput";
 
 function App() {
   return (
     <>
       <Header />
+      <UserInput />
     </>
   );
 }
